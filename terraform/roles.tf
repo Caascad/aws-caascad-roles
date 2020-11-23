ebb8108a89f2deed4652f80e34d56f2bcb85d3e3
Jean-Philippe Braun <eon@patapon.info>
2020-11-23 11:16:54 +0100

    Add AmazonEKSServicePolicy to cluster role

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -26,6 +26,11 @@ resource aws_iam_role_policy_attachment eks_cluster {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource aws_iam_role_policy_attachment eks_cluster_slr {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
 resource aws_iam_role eks_node_group_role {
   name = "caascad_eks_node_group_role"
 
