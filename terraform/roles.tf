5f96cddd78a97efc1f2f1c34dbcdd8ef2eaa075b
Jean-Philippe Braun <eon@patapon.info>
2020-12-07 17:38:14 +0100

    Add policy to cluster role to fix NLB issues

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -21,6 +21,26 @@ resource aws_iam_role eks_cluster_role {
 
 }
 
+// https://medium.com/faun/aws-eks-the-role-is-not-authorized-to-perform-ec2-describeaccountattributes-error-1c6474781b84
+data aws_iam_policy_document eks_cluster_role_custom {
+  statement {
+    actions = [
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeInternetGateways",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource aws_iam_policy eks_cluster_role_custom {
+  name = "caascad-eks-cluster-policy"
+  path = "/"
+
+  policy = data.aws_iam_policy_document.eks_cluster_role_custom.json
+}
+
 resource aws_iam_role_policy_attachment eks_cluster {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -29,6 +49,11 @@ resource aws_iam_role_policy_attachment eks_cluster {
 resource aws_iam_role_policy_attachment eks_cluster_slr {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+resource aws_iam_role_policy_attachment eks_cluster_custom {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = aws_iam_policy.eks_cluster_role_custom.arn
 }
 
 resource aws_iam_role eks_node_group_role {
