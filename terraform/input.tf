cf72cbba8888b55b67c9b1b7cf841294969173f1
stephane teyssier <stephane.teyssier@gmail.com>
2021-04-27 10:13:54 +0200

    Update terraform/input.tf
    Co-authored-by: Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>

--- terraform/input.tf
+++ terraform/input.tf
@@ -6,5 +6,5 @@ variable caascad_operator_trusted_arn {
 variable existing_cluster {
   type        = bool
   default     = false
-  description = "Indicate if the EKS cluster and so the required network (VPC, subnets, ...) is already existing."
+  description = "Indicate if the EKS cluster is already existing."
 }
