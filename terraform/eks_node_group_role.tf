87865a1feda7dea8c8bbcf8ee0f329b905ebc3e0
Karim Oueslati <karim.oueslati.ext@orange.com>
2021-09-21 12:01:45 +0200

    Add policy for cluster nodes auto-scaling

--- terraform/eks_node_group_role.tf
+++ terraform/eks_node_group_role.tf
@@ -17,6 +17,30 @@ resource "aws_iam_role" "eks_node_group_role" {
   }
 }
 
+data "aws_iam_policy_document" "eks_node_group_role_custom" {
+  statement {
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions"
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "eks_node_group_role_custom" {
+  name = "caascad-eks-node-group-policy"
+  path = "/"
+
+  policy = data.aws_iam_policy_document.eks_node_group_role_custom.json
+}
+
 resource "aws_iam_role_policy_attachment" "eks_worker_node" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_node_group_role.name
@@ -30,4 +54,9 @@ resource "aws_iam_role_policy_attachment" "eks_cni" {
 resource "aws_iam_role_policy_attachment" "ec2_container_registry_ro" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_custom" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = aws_iam_policy.eks_node_group_role_custom.arn
 }
